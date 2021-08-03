b8964aaa5b6a821b92c318d8098fb1f9af102082
Manuel Moauro <manuelmoauro1@gmail.com>
2021-08-03 09:00:47 -0300

    Add basic parties methods

--- api/src/parties/parties.controller.ts
+++ api/src/parties/parties.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import { PartiesService } from './parties.service';
+import { Parties } from './parties.entity';
 
 @Controller('parties')
 export class PartiesController {
@@ -7,5 +8,24 @@ export class PartiesController {
   @Get()
   getAll() {
     return this.service.findAll();
+  }
+  @Get(':id')
+  getById(@Param('id') id: number) {
+    return this.service.findById(id);
+  }
+  @Post()
+  async create(@Body('party') party: Parties): Promise<Parties> {
+    await this.service.create(party);
+    return party;
+  }
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    const party = await this.service.findById(id);
+    this.service.delete(party);
+  }
+  @Put()
+  async update(@Body('party') party: Parties): Promise<Parties> {
+    await this.service.update(party);
+    return party;
   }
 }
--- api/src/parties/parties.service.ts
+++ api/src/parties/parties.service.ts
@@ -8,7 +8,21 @@ export class PartiesService {
   constructor(
     @InjectRepository(Parties) private repository: Repository<Parties>,
   ) {}
-  async findAll() {
+  findAll() {
     return this.repository.find({ relations: ['party_category', 'creator'] });
+  }
+  findById(id: number) {
+    return this.repository.findOne(id, {
+      relations: ['party_category', 'creator'],
+    });
+  }
+  create(party: Parties) {
+    this.repository.save(party);
+  }
+  delete(party: Parties) {
+    this.repository.delete(party);
+  }
+  update(party: Parties) {
+    this.repository.update(party.id, party);
   }
 }
